4e54b64aa817e81bf0012704ddf3276877188442
Marco Serrano <marcoTheWebDev@gmail.com>
2021-07-02 00:30:21 -0500

    Replaced the Inbox contract with the Lottery contract and updated Inbox variables to Lottery variables. Replaced test.js file with one that more comprehensively tests Lottery.

--- deploy.js
+++ deploy.js
@@ -15,10 +15,10 @@ const deploy = async () => {
     const accounts = await web3.eth.getAccounts();
     console.log('Attempting to deploy from account', accounts[0]);
 
-    const inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments: ['Hello Rinkeby!'] })
+    const lottery = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode })
         .send({ gas: '1000000', from: accounts[0] });
 
-    console.log('Contract deployed to', inbox.options.address);
+    console.log('Contract deployed to', lottery.options.address);
 };
 deploy();
